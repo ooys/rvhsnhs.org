b28ef6533669444d8a3c8a8ca882e889516fb26e
Yihong Song <1834395537@qq.com>
2021-06-18 18:53:09 -0400

    Initialize Next

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import "../node_modules/bulma/css/bulma.css";
+import "../styles/index.css";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "../node_modules/nprogress/nprogress.css"; //styles of nprogress
+import React, { useEffect } from "react";
+//Binding events.
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+function Index() {
+    const router = useRouter();
+    return <div>Hello World</div>;
+}
+
+export default Index;
