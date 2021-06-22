ac84831530a411d2b683d1534373162ffa371591
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2021-06-21 20:12:51 -0400

    Very basic navbar that I'm still working on. Was trying to get the old one to work because it's more customized but couldn't

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,52 @@ function SignOut() {
 function Index() {
     return (
         <>
+            <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">
+                        <img src="/images/newnhslogo.png"/>
+                    </a>
+
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                         <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item">
+                            Home
+                        </a>
+
+                        <a class="navbar-item">
+                            Apply
+                        </a>
+
+                        <div class="navbar-item">
+                            <a class="navbar-link">
+                                About
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <a class="button is-white">
+                                    Sign up
+                                </a>
+                                <a href="/login" class="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+
             <div>Home</div>
             <Link href="/login">
                 <a>Login</a>
