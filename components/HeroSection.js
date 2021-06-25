491dc7bcd20d9ce753d36bdedc26b9bf57f45f8c
Yihong Song <1834395537@qq.com>
2021-06-25 14:28:47 -0400

    Updated Hero

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function HeroSection() {
+    const router = useRouter();
     return (
         <div className="hero-container">
             <video src="video-1.mp4" autoPlay loop muted />
-            <h1>VOLUNTEER WITH NHS</h1>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                GET STARTED WATCH TRAILER <i className="far fa-play-circle" />
-            </div>
+            <span className="hero-title">National Honor Society</span>
+            <span className="hero-subtitle">Riverside High School Chapter</span>
+            <a
+                className="hero-button"
+                onClick={() => {
+                    router.push("/apply");
+                }}>
+                Join Now
+                <span className="hero-button-icon">
+                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                </span>
+            </a>
+            <img
+                className="hero-logo"
+                src="/images/newnhslogo.png"
+                alt="Riverside NHS"></img>
         </div>
     );
 }
