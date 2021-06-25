5b1e6b1633b97929cd3b1b0995ce0d3a6e23191d
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2021-06-25 12:48:53 -0400

    Video Playing no Buttons
    Got the video to play but there aren't any buttons showing yet. Also the video is messed up on the bottom of the screen for some reason.

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import "firebase/auth";
 import initFirebase from "../services/firebase.js";
 import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection.js";
 
 initFirebase();
 const auth = firebase.auth();
@@ -14,9 +15,7 @@ function Index() {
     return (
         <>
             <Navbar />
-            <div className="page-wrapper" id="index">
-                <div>Home</div>
-            </div>
+            <HeroSection />
             <Footer />
         </>
     );
