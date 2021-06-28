59a2ee2d7cde9c483011dc07e43b688e3b108c6f
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2021-06-28 19:56:25 -0400

    Created Basic Verification Page
    Initialized the page with static information. Would like to get the information to center in the table vertically and switch the images to their respective hover images when hovered over.

--- pages/officer/verify/hours.js
+++ pages/officer/verify/hours.js
@@ -1,5 +1,285 @@
+import Navbar from "/components/Navbar.js";
+
 function Hours() {
-    return <>Hours</>;
+    return (
+        <>
+            <Navbar user="officer" />
+            <div className="page-wrapper" id="admin-group">
+                <div className="verificiation-title">
+                    <p> There are (8) submissions pending confirmation </p>
+                </div>
+                <div className="admin-group-table">
+                    <table className="table is-bordered is-fullwidth profile-hours-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Event Title</th>
+                                <th>Hours Claimed</th>
+                                <th>Verification</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+
+                            <tr>
+                                <td>Johnny Appleseed</td>
+                                <td>RVHS Freshmen Tours</td>
+                                <td>2.0</td>
+                                <td>
+                                    <img
+                                        src="/images/insideriverside.PNG"
+                                        alt=""></img>
+                                </td>
+                                <div className="verify-divider">
+                                    <a
+                                        className="check-button"
+                                        onClick={() => {
+                                            print("Accepted");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/checknohover.png"
+                                            alt=""></img>
+                                    </a>
+
+                                    <a
+                                        className="x-button"
+                                        onClick={() => {
+                                            print("Rejected");
+                                        }}>
+                                        <img
+                                            src="/images/buttons/xnohover.png"
+                                            alt=""></img>
+                                    </a>
+                                </div>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Hours;
