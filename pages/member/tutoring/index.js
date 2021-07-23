5c58dcfb943386005d778d89c5af7231b209c166
Yihong Song <1834395537@qq.com>
2021-07-23 15:12:15 -0400

    Removed Table Library
    Bad library :-1:

--- pages/member/tutoring/index.js
+++ pages/member/tutoring/index.js
@@ -6,25 +6,6 @@ import Footer from "/components/Footer";
 import withAuth from "/components/auth/withAuth.js";
 import { useRouter } from "next/router";
 import { useCollectionOnce } from "react-firebase-hooks/firestore";
-import MaterialTable from "material-table";
-import { forwardRef } from "react";
-import {
-    AddBox,
-    Check,
-    Clear,
-    DeleteOutline,
-    ChevronRight,
-    Edit,
-    SaveAlt,
-    FilterList,
-    FirstPage,
-    LastPage,
-    ChevronLeft,
-    Search,
-    ArrowDownward,
-    Remove,
-    ViewColumn,
-} from "@material-ui/icons";
 
 initFirebase();
 const db = firebase.firestore();
@@ -33,68 +14,12 @@ function TutorSelect() {
     const router = useRouter();
     const tuteeRef = db.collection("tutee-requests");
     const [snapshot, loading, error] = useCollectionOnce(tuteeRef);
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => (
-            <DeleteOutline {...props} ref={ref} />
-        )),
-        DetailPanel: forwardRef((props, ref) => (
-            <ChevronRight {...props} ref={ref} />
-        )),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => (
-            <FirstPage {...props} ref={ref} />
-        )),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => (
-            <ChevronRight {...props} ref={ref} />
-        )),
-        PreviousPage: forwardRef((props, ref) => (
-            <ChevronLeft {...props} ref={ref} />
-        )),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => (
-            <ArrowDownward {...props} ref={ref} />
-        )),
-        ThirdStateCheck: forwardRef((props, ref) => (
-            <Remove {...props} ref={ref} />
-        )),
-        ViewColumn: forwardRef((props, ref) => (
-            <ViewColumn {...props} ref={ref} />
-        )),
-    };
 
     function TuteeCard(tutee) {
         return null;
     }
 
     function DisplayTutees({ tutees }) {
-        let tableData = [];
-        {
-            tutees.docs.map((tutee, index) => {
-                if (tutee.id != "init") {
-                    let tuteeData = tutee.data();
-                    console.log(tuteeData);
-                    tableData.push({
-                        timestamp: new Date(
-                            tuteeData.timestamp.seconds * 1000
-                        ).toString(),
-                        termlength: tuteeData.termlength,
-                        school: tuteeData.tutee.school.name,
-                        grade: tuteeData.tutee.school.grade,
-                        subject: tuteeData.tutee.school.subject,
-                        course: tuteeData.tutee.school.course,
-                        studyhall: tuteeData.availability.studyhall,
-                        exterior: tuteeData.availability.exterior,
-                    });
-                }
-            });
-        }
         const subjects = [
             "English",
             "Social Sciences & History",
@@ -105,50 +30,76 @@ function TutorSelect() {
             "Music",
         ];
         return (
-            <div className="tutee-table-wrapper">
-                <MaterialTable
-                    icons={tableIcons}
-                    title="Select Tutee"
-                    columns={[
-                        // { title: "Timestamp", field: "timestamp" },
-                        { title: "Term Length", field: "termlength" },
-                        { title: "School", field: "school" },
-                        {
-                            title: "Grade",
-                            field: "grade",
-                        },
-                        {
-                            title: "Subject",
-                            field: "subject",
-                            lookup: {
-                                English: "English",
-                                "Social Sciences & History":
-                                    "Social Sciences & History",
-                                Mathematics: "Mathematics",
-                                Science: "Science",
-                                "World Languages": "World Languages",
-                                Arts: "Arts",
-                                Music: "Music",
-                            },
-                        },
-                        {
-                            title: "Course",
-                            field: "course",
-                        },
-                        {
-                            title: "Study Hall",
-                            field: "studyhall",
-                        },
-                        {
-                            title: "Availabilities",
-                            field: "exterior",
-                        },
-                    ]}
-                    data={tableData}
-                    options={{
-                        filtering: true,
-                    }}
-                />
+            <div className="columns is-multiline tutee-list">
+                {tutees.docs.map((tutee, index) => {
+                    if (tutee.id != "init") {
+                        let tuteeData = tutee.data();
+                        return (
+                            <div
+                                key={index}
+                                className="column is-full tutee-card-wrapper">
+                                <div className="columns is-multiline is-mobile tutee-card is-gapless">
+                                    <div className="column is-full tutee-card-title">
+                                        {tuteeData.tutee.first +
+                                            ", " +
+                                            tuteeData.tutee.last}
+                                    </div>
+                                    <div className="column is-half tutee-card-body">
+                                        <div className="columns tutee-card-left is-multiline is-gapless">
+                                            <div className="column is-full tutee-card-left-text">
+                                                {tuteeData.tutee.school.name}
+                                            </div>
+                                            <div className="column is-full tutee-card-left-text">
+                                                {"Grade " +
+                                                    tuteeData.tutee.school
+                                                        .grade}
+                                            </div>
+                                            <div className="column is-full tutee-card-left-text">
+                                                {tuteeData.termlength}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="column is-half tutee-card-body">
+                                        <div className="columns tutee-card-left is-multiline is-gapless">
+                                            <div className="column is-full tutee-card-left-text">
+                                                <b>
+                                                    {
+                                                        tuteeData.tutee.school
+                                                            .subject
+                                                    }
+                                                </b>
+                                            </div>
+                                            <div className="column is-full tutee-card-left-text">
+                                                {tuteeData.tutee.school.course}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="column is-full tutee-card-tags">
+                                        Availabilities:{" "}
+                                        {tuteeData.availability.studyhall.toString() ===
+                                        "None" ? null : (
+                                            <span className="tag tutee-card-tag">
+                                                {
+                                                    tuteeData.availability
+                                                        .studyhall
+                                                }
+                                            </span>
+                                        )}
+                                        {tuteeData.availability.exterior.map(
+                                            (time, index) => {
+                                                return (
+                                                    <span className="tag tutee-card-tag">
+                                                        {time}
+                                                    </span>
+                                                );
+                                            }
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
             </div>
         );
     }
