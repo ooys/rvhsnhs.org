d42b1156beed8fca88b46402c3c23c3d06c44d27
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2021-07-24 23:59:51 -0400

    Began Member Index Mobile
    Started optimizing for different screen sizes on the member index page. Work in progress.

--- pages/member/index.js
+++ pages/member/index.js
@@ -62,29 +62,113 @@ function Member() {
             <div className="page-wrapper" id="member">
                 <div className="columns columns-wrapper is-multiline">
                     <div className="column member-main-section is-full is-bg-off-white">
-                        <div className="column totals-column is-10 is-offset-1 is-bg-off-white is-multiline">
-                            <div className="column total-column-1 is-bg-light"></div>
-                            <div className="column total-column-2 is-bg-light"></div>
-                            <div className="column total-column-3 is-bg-light"></div>
-                            <div className="column total-column-4 is-bg-light"></div>
+                        <div className="columns totals-column is-bg-off-white">
+                            <div className="column total-column-1 is-bg-light is-10-mobile is-offset-1-mobile">
+                                <div className="column total-column-1-mixed ">
+                                    Total Mixed
+                                </div>
+                                <div className="columns mixed-hours-content is-multiline is-mobile">
+                                    <img
+                                        className="mixed-hours-icon"
+                                        src="images\membertotals\volunteericon.svg"
+                                    />
+                                    <div className="column current-mixed-hours-progress">
+                                        <div className="current-mixed-hours-number">
+                                            8
+                                        </div>
+                                        <div className=" current-mixed-hours-bar">
+                                            <img
+                                                className="progress-bar-icon"
+                                                src="images\membertotals\progressbar.png"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column total-column-1-semester">
+                                    This Semester
+                                </div>
+                            </div>
+                            <div className="column total-column-2 is-bg-light is-10-mobile is-offset-1-mobile">
+                                <div className="column total-column-1-mixed">
+                                    Total Tutoring
+                                </div>
+                                <div className="columns mixed-hours-content is-multiline is-mobile">
+                                    <img
+                                        className="mixed-hours-icon"
+                                        src="images\membertotals\classicon.svg"
+                                    />
+                                    <div className="column current-mixed-hours-progress">
+                                        <div className="current-mixed-hours-number">
+                                            90
+                                        </div>
+                                        <div className=" current-mixed-hours-bar">
+                                            <img
+                                                className="progress-bar-icon"
+                                                src="images\membertotals\progressbar.png"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column total-column-1-semester">
+                                    This Semester
+                                </div>
+                            </div>
+                            <div className="column total-column-3 is-bg-light is-10-mobile is-offset-1-mobile">
+                                <div className="column total-column-1-mixed">
+                                    Total Hours
+                                </div>
+                                <div className="columns mixed-hours-content is-multiline is-mobile">
+                                    <img
+                                        className="mixed-hours-icon"
+                                        src="images\membertotals\timeicon.svg"
+                                    />
+                                    <div className="column current-mixed-hours-progress">
+                                        <div className="current-mixed-hours-number">
+                                            90
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column total-column-1-semester">
+                                    This Semester
+                                </div>
+                            </div>
+                            <div className="column total-column-4 is-bg-light is-10-mobile is-offset-1-mobile">
+                                <div className="column total-column-1-mixed">
+                                    Days Left
+                                </div>
+                                <div className="columns mixed-hours-content is-multiline is-mobile">
+                                    <img
+                                        className="mixed-hours-icon"
+                                        src="images\membertotals\calendaricon.svg"
+                                    />
+                                    <div className="column current-mixed-hours-progress">
+                                        <div className="current-mixed-hours-number">
+                                            90
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column total-column-1-semester">
+                                    This Semester
+                                </div>
+                            </div>
                         </div>
-                        <div className="column member-current-section is-10 is-offset-1 is-bg-off-white is-multiline">
+                        <div className="columns member-current-section is-10 is-offset-1 is-bg-off-white is-multiline ">
                             <div className="column member-graph-container is-three-fifths is-bg-light"></div>
                             <div className="column member-current-rank is-bg-light"></div>
                         </div>
                         <div className="column featured-opportunities-text-container is-10 is-offset-1 is-bg-off-white">
                             Featured Opportunities
                         </div>
-                        <div className="column featured-opportunities is-10 is-offset-1 is-bg-off-white">
+                        <div className="columns featured-opportunities is-10 is-offset-1 is-bg-off-white">
                             <div className=" member-left-slideshow-arrow is-1">
                                 <img
                                     src="/images/slideshow_arrows/left-arrow.svg"
                                     alt=""></img>
                             </div>
-                            <div className="column featured-opportunity-1 is-bg-light"></div>
-                            <div className="column featured-opportunity-2 is-bg-light"></div>
-                            <div className="column featured-opportunity-3 is-bg-light"></div>
-                            <div className="column featured-opportunity-4 is-bg-light"></div>
+                            <div className="column featured-opportunity-1 is-bg-light is-10-mobile is-offset-1-mobile"></div>
+                            <div className="column featured-opportunity-2 is-bg-light is-10-mobile is-offset-1-mobile"></div>
+                            <div className="column featured-opportunity-3 is-bg-light is-10-mobile is-offset-1-mobile"></div>
+                            <div className="column featured-opportunity-4 is-bg-light is-10-mobile is-offset-1-mobile"></div>
                             <div className=" member-right-slideshow-arrow is-1">
                                 <img
                                     src="/images/slideshow_arrows/right-arrow.svg"
@@ -93,7 +177,6 @@ function Member() {
                         </div>
                     </div>
                 </div>
-                <img src="/images/mainpage.png" alt=""></img>
             </div>
             <Footer />
         </div>
