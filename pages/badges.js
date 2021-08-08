8b4fb9e16d8246a0e60444c7366ec19178ce78a3
Yihong Song <1834395537@qq.com>
2021-08-07 21:25:07 -0400

    Fixed Badges a Little

--- pages/badges.js
+++ pages/badges.js
@@ -1,86 +1,96 @@
 import Navbar from "/components/Navbar";
 import Footer from "/components/Footer";
-import router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import Steps, { Step } from "rc-steps";
 import { useState } from "react";
 import swal from "sweetalert";
-
-function BadgeModal(props) {
-    if (props.active === props.title) {
-        return (
-            <div className={"modal is-active"} id="badge-modal">
-                <div
-                    className="modal-background"
-                    onClick={() => {
-                        props.setActive("");
-                    }}></div>
-                <div className="modal-content" id="badge-modal-content">
-                    <div className="columns is-mobile is-variable is-4 modal-wrapper">
-                        <div className="column is-one-third modal-picture">
-                            <img
-                                className={"modal-badge " + props.color}
-                                src={props.src}
-                                alt={props.title}></img>
-                        </div>
-                        <div className="column is-two-thirds modal-text">
-                            <div className="modal-title">{props.title}</div>
-                            <hr></hr>
-                            <div className="modal-desc">{props.desc}</div>
-                        </div>
-                    </div>
-                </div>
-                <button
-                    className="modal-close is-large"
-                    aria-label="close"
-                    onClick={() => {
-                        props.setActive("");
-                    }}></button>
-            </div>
-        );
-    } else {
-        return <></>;
-    }
-}
-
-function Badge(name, active, setActive) {
-    let src = "";
-    let desc = "";
-    let title = "";
-    switch (name) {
-        case "officer":
-            title = "Officer";
-            desc = "Is an National Honor Society Officer.";
-            src = "/images/badges/officer.svg";
-            break;
-        case "beta_tester":
-            title = "Beta Tester";
-            desc =
-                "Helped test the National Honor Society Website in its beta stages.";
-            src = "/images/badges/beta_tester.svg";
-            break;
-        case "bug_hunter":
-            title = "Bug Hunter";
-            desc = "Found and reported a significant bug.";
-            src = "/images/badges/bug_hunter.svg";
-            break;
-        case "overachiever":
-            title = "Overachiever";
-            desc = "Logged in during a school break.";
-            src = "/images/badges/overachiever.svg";
-            break;
-        case "volunteer_of_the_month":
-            title = '"Volunteer of the Month" Award';
-            desc = "Selected as Volunteer of the Month by the NHS committee.";
-            src = "/images/badges/medal.svg";
-            break;
-    }
-}
 
 function Badges() {
     const [active, setActive] = useState("");
     const router = useRouter();
+
+    function BadgeModal({ name, title, src, desc, color }) {
+        if (active === name) {
+            return (
+                <div className={"modal is-active"} id="badge-modal">
+                    <div
+                        className="modal-background"
+                        onClick={() => {
+                            setActive("");
+                        }}></div>
+                    <div className="modal-content" id="badge-modal-content">
+                        <div className="columns is-mobile is-variable is-4 modal-wrapper">
+                            <div className="column is-one-third modal-picture">
+                                <img
+                                    className={"modal-badge " + color}
+                                    src={src}
+                                    alt={title}></img>
+                            </div>
+                            <div className="column is-two-thirds modal-text">
+                                <div className="modal-title">{title}</div>
+                                <hr></hr>
+                                <div className="modal-desc">{desc}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        className="modal-close is-large"
+                        aria-label="close"
+                        onClick={() => {
+                            setActive("");
+                        }}></button>
+                </div>
+            );
+        } else {
+            return <></>;
+        }
+    }
+
+    function Badge({ name }) {
+        let src = "";
+        let desc = "";
+        let title = "";
+        switch (name) {
+            case "officer":
+                title = "Officer";
+                desc = "Is an National Honor Society Officer.";
+                src = "/images/badges/officer.svg";
+                break;
+            case "beta_tester":
+                title = "Beta Tester";
+                desc =
+                    "Helped test the National Honor Society Website in its beta stages.";
+                src = "/images/badges/beta_tester.svg";
+                break;
+            case "bug_hunter":
+                title = "Bug Hunter";
+                desc = "Found and reported a significant bug.";
+                src = "/images/badges/bug_hunter.svg";
+                break;
+            case "overachiever":
+                title = "Overachiever";
+                desc = "Logged in during a school break.";
+                src = "/images/badges/overachiever.svg";
+                break;
+            case "volunteer_of_the_month":
+                title = '"Volunteer of the Month" Award';
+                desc =
+                    "Selected as Volunteer of the Month by the NHS committee.";
+                src = "/images/badges/medal.svg";
+                break;
+        }
+        return (
+            <BadgeModal
+                name={name}
+                title={title}
+                src={src}
+                desc={desc}
+                color={""}
+            />
+        );
+    }
+
     return (
         <>
             <Navbar />
@@ -91,12 +101,15 @@ function Badges() {
                             className={
                                 "columns about-section-wrapper is-vcentered is-multiline is-6"
                             }>
-                            <div className="column findtutor-about-section-text is-full">
+                            <div className="column is-full">
                                 <div className="badges-section-header">
                                     Badges
                                 </div>
                                 <div className="badges-section-body">
-                                    There are a ton of badges you can earn through your involvment in NHS. Click on a badge to learn more about it and how you can earn it.
+                                    There are a ton of badges you can earn
+                                    through your involvment in NHS. Click on a
+                                    badge to learn more about it and how you can
+                                    earn it.
                                 </div>
                                 <br></br>
                                 <div className="columns badge-list is-multiline">
@@ -104,29 +117,29 @@ function Badges() {
                                         <a
                                             className="column listed-badge-wrapper"
                                             onClick={() => {
-                                                setActive(officer);
+                                                setActive("officer");
                                             }}>
-                                            <img className="badge-pic" src="/images/badges/officer.svg" alt="Officer"></img>
+                                            <img
+                                                className="badge-pic"
+                                                src="/images/badges/officer.svg"
+                                                alt="Officer"></img>
                                         </a>
+                                        <Badge name={"officer"} />
                                         <div className="listed-badge-name">
                                             Officer
                                         </div>
                                     </div>
-                                    <div className="column listed-badge is-one-third">
+                                    {/* <div className="column listed-badge is-one-third">
                                         <a
                                             className="column listed-badge-wrapper"
                                             onClick={() => {
-                                                setActive("officer");
+                                                setActive("beta_tester");
                                             }}>
-                                            <img className="badge-pic" src="/images/badges/beta_tester.svg" alt="Beta Tester"></img>
+                                            <img
+                                                className="badge-pic"
+                                                src="/images/badges/beta_tester.svg"
+                                                alt="Beta Tester"></img>
                                         </a>
-                                        <BadgeModal
-                                            active={active}
-                                            title="officer"
-                                            desc="Is an National Honor Society Officer."
-                                            src="/images/badges/beta_tester.svg"
-                                            setActive={setActive}
-                                        />
                                         <div className="listed-badge-name">
                                             Beta Tester
                                         </div>
@@ -137,7 +150,10 @@ function Badges() {
                                             onClick={() => {
                                                 setActive(title);
                                             }}>
-                                            <img className="badge-pic" src="/images/badges/bug_hunter.svg" alt="Bug Hunter"></img>
+                                            <img
+                                                className="badge-pic"
+                                                src="/images/badges/bug_hunter.svg"
+                                                alt="Bug Hunter"></img>
                                         </a>
                                         <div className="listed-badge-name">
                                             Bug Hunter
@@ -149,7 +165,10 @@ function Badges() {
                                             onClick={() => {
                                                 setActive(title);
                                             }}>
-                                            <img className="badge-pic" src="/images/badges/overachiever.svg" alt="Overachiever"></img>
+                                            <img
+                                                className="badge-pic"
+                                                src="/images/badges/overachiever.svg"
+                                                alt="Overachiever"></img>
                                         </a>
                                         <div className="listed-badge-name">
                                             Overachiever
@@ -161,24 +180,19 @@ function Badges() {
                                             onClick={() => {
                                                 setActive(title);
                                             }}>
-                                            <img className="badge-pic" src="/images/badges/medal.svg" alt="Volunteer of the Month"></img>
+                                            <img
+                                                className="badge-pic"
+                                                src="/images/badges/medal.svg"
+                                                alt="Volunteer of the Month"></img>
                                         </a>
                                         <div className="listed-badge-name">
                                             Volunteer of the Month
                                         </div>
                                     </div>
-                                    <div className="column listed-badge is-one-third">
-                                        
-                                    </div>
-                                    <div className="column listed-badge is-one-third">
-                                        
-                                    </div>
-                                    <div className="column listed-badge is-one-third">
-                                        
-                                    </div>
-                                    <div className="column listed-badge is-one-third">
-                                        
-                                    </div>
+                                    <div className="column listed-badge is-one-third"></div>
+                                    <div className="column listed-badge is-one-third"></div>
+                                    <div className="column listed-badge is-one-third"></div>
+                                    <div className="column listed-badge is-one-third"></div> */}
                                 </div>
                             </div>
                             {/* <div className="column about-section-text is-full">
@@ -210,7 +224,7 @@ function Badges() {
                         </div>
                     </div>
                 </div>
-                
+
                 {/* <div className="columns">
                     <div className="column">
                         <div className="column is-vcentered">
